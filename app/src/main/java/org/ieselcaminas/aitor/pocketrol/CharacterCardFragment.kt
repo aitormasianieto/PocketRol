14137f065dfce4a36638fe7364f58a5f68c53934
Aitor <masianieto98@gmail.com>
2020-01-10 20:41:31 +0100

    1st Commit

---
+++ app/src/main/java/org/ieselcaminas/aitor/pocketrol/CharacterCardFragment.kt
@@ -0,0 +1,20 @@
+package org.ieselcaminas.aitor.pocketrol
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import org.ieselcaminas.aitor.pocketrol.databinding.FragmentCharacterCardBinding
+
+
+class CharacterCardFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Get a reference to the binding object and inflate the fragment views.
+        val binding: FragmentCharacterCardBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_character_card, container, false)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/org/ieselcaminas/aitor/pocketrol/CharacterCardViewModel.kt
@@ -0,0 +1,7 @@
+package org.ieselcaminas.aitor.pocketrol
+
+import androidx.lifecycle.ViewModel
+
+class CharacterCardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
